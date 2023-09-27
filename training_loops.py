9721551b00cb0e23302be908e30b7d182ec983ef
Deceptrax123 <srinitishsri@gmail.com>
2023-09-28 00:28:34 +0530

    create train and test dataset and loader class

---
+++ training_loops.py
@@ -0,0 +1,52 @@
+import torch 
+import torchvision.transforms.v2 as T 
+from torch.utils.data import DataLoader
+from wheat_dataset import WheatDataset
+import torch.multiprocessing
+import wandb
+from time import time 
+from torch import mps,cpu 
+import gc 
+from sklearn.model_selection import train_test_split
+import pandas as pd 
+
+
+if __name__=='__main__':
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    dataset=pd.read_csv("./train/train.csv")
+
+    image_names=list(dataset['image_name'])
+    bounding_boxes=list(dataset['BoxesString'])
+
+    data=dict(zip(image_names,bounding_boxes))
+
+    train,test=train_test_split(image_names,test_size=0.25)
+
+
+    train_set=WheatDataset(image_names=train,data_dict=data)
+    test_set=WheatDataset(image_names=test,data_dict=data)
+
+    params_train={
+        'batch_size':8,
+        'shuffle':True,
+        'num_workers':0,
+        'collate_fn':train_set.collate_fn
+    }
+
+    params_test={
+        'batch_size':8,
+        'shuffle':True,
+        'num_workers':0,
+        'collate_fn':test_set.collate_fn
+    }
+
+    train_loader=DataLoader(train_set,**params_train)
+    test_loader=DataLoader(test_set,**params_test)
+
+    #test the dataloader
+    #print(train)
+    for i,(img,box) in enumerate(train_loader):
+        print(box)
+
+        if i==0:
+            break
