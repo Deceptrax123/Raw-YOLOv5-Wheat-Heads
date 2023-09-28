6ae600a116ddf4ba05aa2ad2f3d2093ded606c96
Deceptrax123 <srinitishsri@gmail.com>
2023-09-29 01:32:51 +0530

    annotations generation script

---
+++ create_data_folders.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd 
+import os 
+from sklearn.model_selection import train_test_split
+
+def split_dataset():
+    pass
--- generate_annotations.py
+++ generate_annotations.py
@@ -3,7 +3,14 @@ import pandas as pd
 import numpy as np 
 from PIL import Image
 from sklearn.model_selection import train_test_split
+import os 
 
+#global variables
+
+mapping={
+    "wheat_head":1,
+    "background":0
+}
 
 def create_dict():
     data=pd.read_csv("./train/train.csv")
@@ -58,3 +65,36 @@ def create_dict():
             break
 
     return data_details
+
+def prepare_for_yolov5(data_details):
+
+    file_list=list()
+    for box in data_details['bboxes']:
+
+        class_id=mapping[box['class']]
+        #Transform to yolo v5 format
+        box_center_x=(box['xmin'])+(box['xmax'])/2
+        box_center_y=(box['ymin']+box['ymax'])/2
+        box_width=box['xmax']-box['xmin']
+        box_height=box['ymax']-box['ymin']
+
+        h,w,c=data_details['image_size']
+
+        box_center_x=box_center_x/h
+        box_center_y=box_center_y/w
+        box_width=box_width/h
+        box_height=box_height/w
+
+        file_list.append("{} {:.3f} {:.3f} {:.3f}".format(class_id,box_center_x,box_center_y,box_width,box_height))
+    
+    #save the coordinates to a text file as required by Yolo v5
+    file_name="./annotations/"+data_details['filename'].replace("png","txt")
+    
+    #write annotations to file
+    with open(file_name,'w') as f:
+        for i,item in enumerate(file_list):
+            f.write(item)
+            
+            if i!=len(file_list)-1:
+                f.write("\n")
+        f.close()
